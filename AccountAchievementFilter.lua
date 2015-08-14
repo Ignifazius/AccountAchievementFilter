1c3733ac1c527d64868dcefa996cd049400dc8fc
Ignifazius <curse@trollstar-gaming.de>
2015-08-15 00:55:43 +0200

    adding some comments to the code & increase version to 1.2 for next release

--- AccountAchievementFilter.lua
+++ AccountAchievementFilter.lua
@@ -1,6 +1,6 @@
 local _, L = ...;
-
-local frame = CreateFrame ("Button", "AccAvFFrame", UIParent)
+cryForHelp = false;
+local frame = CreateFrame ("Button", "AccAvFFrame", UIParent) -- create a Frame (doesnt Matter which one) to start a function
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function (self,event,...)
 	if event == "ADDON_LOADED" then
@@ -9,24 +9,24 @@ frame:SetScript("OnEvent", function (self,event,...)
 end)
 
 function addFourthOption()
-	ACHIEVEMENT_FILTER_ACCOUNT_INCOMPLETE = 4;
-	ACHIEVEMENTFRAME_FILTER_ACCOUNT_INCOMPLETE = L["Account Incomplete"];
-	local original_AchievementFrameFilters = AchievementFrameFilters;
-	AchievementFrameFilters = { 
-		{text=ACHIEVEMENTFRAME_FILTER_ALL, func= AchievementFrame_GetCategoryNumAchievements_All},
-		{text=ACHIEVEMENTFRAME_FILTER_COMPLETED, func=AchievementFrame_GetCategoryNumAchievements_Complete},
-		{text=ACHIEVEMENTFRAME_FILTER_INCOMPLETE, func=AchievementFrame_GetCategoryNumAchievements_Incomplete} ,
-		{text=ACHIEVEMENTFRAME_FILTER_ACCOUNT_INCOMPLETE, func=AchievementFrame_GetCategoryNumAchievements_AccIncomplete}
+	ACHIEVEMENT_FILTER_ACCOUNT_INCOMPLETE = 4; -- add option 4
+	ACHIEVEMENTFRAME_FILTER_ACCOUNT_INCOMPLETE = L["Account Incomplete"]; --set text for dropdownmenu of option 4
+	local original_AchievementFrameFilters = AchievementFrameFilters;	--save original
+	AchievementFrameFilters = {  --replace original with extended version
+		{text=ACHIEVEMENTFRAME_FILTER_ALL, func= AchievementFrame_GetCategoryNumAchievements_All}, --original
+		{text=ACHIEVEMENTFRAME_FILTER_COMPLETED, func=AchievementFrame_GetCategoryNumAchievements_Complete}, --original
+		{text=ACHIEVEMENTFRAME_FILTER_INCOMPLETE, func=AchievementFrame_GetCategoryNumAchievements_Incomplete} , --original
+		{text=ACHIEVEMENTFRAME_FILTER_ACCOUNT_INCOMPLETE, func=AchievementFrame_GetCategoryNumAchievements_AccIncomplete} --new
 		};
 	
-	ACHIEVEMENT_FILTER_ACCOUNT_INCOMPLETE_EXPLANATION = L["Show all achievements not already completed by this ACCOUNT"];
+	ACHIEVEMENT_FILTER_ACCOUNT_INCOMPLETE_EXPLANATION = L["Show all achievements not already completed by this ACCOUNT"]; --set explanation text for option 4
 	
-	local orig_AchievementFrameFilterStrings = AchievementFrameFilterStrings;
-	AchievementFrameFilterStrings = {ACHIEVEMENT_FILTER_ALL_EXPLANATION, 
+	local orig_AchievementFrameFilterStrings = AchievementFrameFilterStrings; --save original
+	AchievementFrameFilterStrings = {ACHIEVEMENT_FILTER_ALL_EXPLANATION, --replace original with extended version
 ACHIEVEMENT_FILTER_COMPLETE_EXPLANATION, ACHIEVEMENT_FILTER_INCOMPLETE_EXPLANATION, ACHIEVEMENT_FILTER_ACCOUNT_INCOMPLETE_EXPLANATION};
 end
 		
-function AchievementFrame_GetCategoryNumAchievements_AccIncomplete(category)
+function AchievementFrame_GetCategoryNumAchievements_AccIncomplete(category) --create function for option 4
 	local total, _, missing = GetCategoryNumAchievements (category);
 	if (not AchievementFrame_IsFeatOfStrength()) then
 		for i = total - missing + 1, total do
