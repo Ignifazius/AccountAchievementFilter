73af440ce1db389e76c7b42b3b712293ae332dc9
Ignifazius <curse@trollstar-gaming.de>
2015-08-17 14:56:40 +0200

    fixed missing enGB

--- localization.en.lua
+++ localization.en.lua
@@ -1,5 +1,5 @@
 local _, L = ...;
-if GetLocale() == "enUS" then
+if GetLocale() == ("enUS" or "enGB") then
 	L["Show all achievements not already completed by this ACCOUNT"] = "Show all achievements not already completed by this ACCOUNT";
 	L["Account Incomplete"] = "Account Incomplete";
 end
