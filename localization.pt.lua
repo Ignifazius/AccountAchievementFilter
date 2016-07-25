726271e5cab1fe34c9c60437ea45ec8f3b0fa0cc
Stefan Hobohm <s.hobohm@tu-bs.de>
2016-07-25 19:14:26 +0200

    fixed ptBR encoding

--- localization.pt.lua
+++ localization.pt.lua
@@ -1,16 +1,16 @@
 local _, L = ...;
 if GetLocale() == "ptBR" then
-	L["Show all achievements not already completed by this ACCOUNT"] = "Mostrar todas as conquistas que não foram concluídas nesta CONTA";
+	L["Show all achievements not already completed by this ACCOUNT"] = "Mostrar todas as conquistas que nÃ£o foram concluÃ­das nesta CONTA";
 	L["Account Incomplete"] = "Incompletos na Conta";
-	L["|cff33FFFF AccountAchievementFilter|r: AV fix|cff33FF33 enabled|r. It will become active after you reload/relog."] = "|cff33FFFF AccountAchievementFilter|r: Correção de CONQ|cff33FF33 Ativado|r. Terá efeito após você recarregar IU.";
-	L["|cff33FFFF AccountAchievementFilter|r: AV fix|cffFF0000 disabled|r. It will become active after you reload/relog."] = "|cff33FFFF AccountAchievementFilter|r: Correção de CONQ|cffFF0000 Desativado|r. Terá efeito após você recarregar IU.";
-	L["Welcome to|cff33FFFF AccountAchievementFilter|r! There is a bug affecting the |cffFF9933incomplete|r option in your achievements. You can enable a fix from the option menu of this addon (currently"] = "Bem-vindo ao|cff33FFFF AccountAchievementFilter|r! Há uma falha que afeta a opção|cffFF9933 Incompleto|r em suas conquistas. Você pode ativar a correção no menu de opções deste addon (";
+	L["|cff33FFFF AccountAchievementFilter|r: AV fix|cff33FF33 enabled|r. It will become active after you reload/relog."] = "|cff33FFFF AccountAchievementFilter|r: CorreÃ§Ã£o de CONQ|cff33FF33 Ativado|r. TerÃ¡ efeito apÃ³s vocÃª recarregar IU.";
+	L["|cff33FFFF AccountAchievementFilter|r: AV fix|cffFF0000 disabled|r. It will become active after you reload/relog."] = "|cff33FFFF AccountAchievementFilter|r: CorreÃ§Ã£o de CONQ|cffFF0000 Desativado|r. TerÃ¡ efeito apÃ³s vocÃª recarregar IU.";
+	L["Welcome to|cff33FFFF AccountAchievementFilter|r! There is a bug affecting the |cffFF9933incomplete|r option in your achievements. You can enable a fix from the option menu of this addon (currently"] = "Bem-vindo ao|cff33FFFF AccountAchievementFilter|r! HÃ¡ uma falha que afeta a opÃ§Ã£o|cffFF9933 Incompleto|r em suas conquistas. VocÃª pode ativar a correÃ§Ã£o no menu de opÃ§Ãµes deste addon (";
 	L["enabled"] = "Ativado"
 	L["disabled"] = "Desativado"
 	L["Use AV \"incomplete\" fix"] = "Usar reparo de CONQ \"incompleta\"";
-	L["Fixes a current bug in GetCategoryNumAchievements(). This MAY break stuff in future blizzard updates."] = "Repara a falha atual em GetCategoryNumAchievements(). Isto pode danificar algumas coisas em futuras atualizações da Blizzard.";
+	L["Fixes a current bug in GetCategoryNumAchievements(). This MAY break stuff in future blizzard updates."] = "Repara a falha atual em GetCategoryNumAchievements(). Isto pode danificar algumas coisas em futuras atualizaÃ§Ãµes da Blizzard.";
 	L["Display welcome message"] = "Exibir mensagem de boas vindas";
-	L["Enables the display of the welcome message after login."] = "Mostra uma mensagem de boas vindas após o login.";
+	L["Enables the display of the welcome message after login."] = "Mostra uma mensagem de boas vindas apÃ³s o login.";
 end
 
 
